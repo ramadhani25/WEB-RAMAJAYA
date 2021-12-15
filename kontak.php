04dd0f6ec599195709d2d229ebd8f5efebf51dad
ramadhani25 <ramadhaniak1125@gmail.com>
2021-12-15 08:13:15 +0700

    edit head

--- kontak.php
+++ kontak.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RAMAJAYA</title>
+    <title>RAMAJAYA - Kontak</title>
+    <link rel="shortcut icon" href="./Assets/icon-leaf.png" type="image/x-icon">
     <link rel="stylesheet" href="./CSS/bootstrap.css">
     <link rel="stylesheet" href="./CSS/style.css">
 </head>
--- moluskisida.php
+++ moluskisida.php
@@ -16,7 +16,8 @@ $d2 = DB::query('SELECT * FROM jenis_barang');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RAMAJAYA</title>
+    <title>RAMAJAYA - Produk</title>
+    <link rel="shortcut icon" href="./Assets/icon-leaf.png" type="image/x-icon">
     <link rel="stylesheet" href="./CSS/bootstrap.css">
     <link rel="stylesheet" href="./CSS/style.css">
 </head>
